3d39f6a9546148689f68b6407a9fc655cb161a44
freitaslivia <xliviafreitasx@gmail.com>
2023-10-27 08:11:03 -0300

    Estilizando cabecalho com tailwind.

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,20 +1,14 @@
 import Link from "next/link";
 
-import React from 'react'
-
 export default function Cabecalho() {
   return (
-
-    <header className="bg-gray-700 text-white h-20">
-    <nav className="flex flex-row justify-around">
-      <Link href="/" className="block py-6 hover:bg-black font-extrabold">HOME</Link>
-      <Link href="/produtos/mamao" className="block py-6 hover:bg-black font-extrabold">MAMÃO</Link>
-      <Link href="/produtos/melao" className="block py-6 hover:bg-black font-extrabold">MELÃO</Link> 
-      <Link href="/produtos/manga" className="block py-6 hover:bg-black font-extrabold">MANGA</Link>
+    <header className='cabecalho'>
+    <nav>
+      <Link href="/">HOME</Link>
+      <Link href="/produtos/mamao" >MAMÃO</Link>
+      <Link href="/produtos/melao" >MELÃO</Link>
+      <Link href="/produtos/manga" >MANGA</Link>
     </nav>
-    </header>
-   
+  </header>
   )
 }
-
-
